151d8d3f9fed66f9c3dace815284d5b784df38dd
DMTris <clangdale5309@gmail.com>
2017-02-04 10:09:57 -0700

    Answer installing/index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+export const name = 'Chris Langdale';
+
+export const email = 'clangdale5309@gmail.com';
+
+export const phone = '801-555-5555';
+
+// Write code here
+
+console.log(name);
+console.log(email);
+console.log(phone);

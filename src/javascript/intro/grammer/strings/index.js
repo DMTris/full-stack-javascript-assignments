1e1875b673461da6b9b87171cf52f9293bb987f1
DMTris <clangdale5309@gmail.com>
2017-02-10 16:29:48 -0700

    Add my answers to index.js

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,1 +1,12 @@
-export const greeting = '';
+export const greeting = 'Hello World';
+export const characterAt = greeting.charAt(4);
+export const greetingArray = greeting.split('');
+export const begins = (str, char) => {
+  if(str[0] == char){
+    return true;
+  }
+};
+export const beginsInsensitive = (str, char) => { if(str[0].toUpperCase() == char.toUpperCase()){
+  return true;
+}
+};

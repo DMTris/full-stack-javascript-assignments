df442b01b58372f2de3a1c869d4cca52e819aac5
DMTris <clangdale5309@gmail.com>
2017-03-20 17:36:38 -0600

    Add greeting function and isTacoTuesday function

--- src/javascript/intro/grammer/control-flow/conditionals/index.js
+++ src/javascript/intro/grammer/control-flow/conditionals/index.js
@@ -1,8 +1,17 @@
-export const greeting = (age = 0) => {
-
+//question response:  We tend to use an if, else style of language. "If you don't do your homework then you get in trouble"
+//greeting function
+export const greeting = (age) => {
+  if (age > 18){
+    return 'Hello!'
+  } else {
+    return 'Yo!'
+  };
 };
-
-export const isTacoTuesday = (day = '') => {
-
+//isTacoTuesday function
+export const isTacoTuesday = (day) => {
+  if (day === 'tuesday'){
+    return true
+  } else {
+    return false
+  };
 };
-
